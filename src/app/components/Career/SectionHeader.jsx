59c22b9095b363a7c47d5bd495eefb3ea8694fe3
John Paul Quintana <jpquintana2024@gmail.com>
2025-01-03 10:23:18 +0800

    Update SectionHeader.jsx

--- src/app/components/Career/SectionHeader.jsx
+++ src/app/components/Career/SectionHeader.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const SectionHeader = () => {
     return (
         <section className="page-intro section-padding desktop:px-40">
-            <div className="text-center text-6xl">
+            <div className="text-center text-3xl phone:text-4xl tablet:text-6xl">
                 <h1>Unlock Your Future: Find Your <span className="font-bold text-color-primary-blue">Perfect Job</span> Today!</h1>
             </div>
         </section>
